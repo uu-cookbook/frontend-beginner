e2cca94b6abb0f68963ab53998b8f1a2922552ee
Petr <petrhaken2@gmail.com>
2023-04-19 17:05:55 +0200

    form 0.0.1

--- src/Components/Small Components/StepForm.js
+++ src/Components/Small Components/StepForm.js
@@ -10,30 +10,39 @@ import { mdiDeleteForever } from "@mdi/js";
 
 import { useState } from "react";
 //import { useId } from "react";
+let id = 0
+let ISdeteletd=false
 
 function StepFrom() {
   //Step logic
-  const [Stepts, addStep] = useState([{ componentId: 12, content: "HELOO" }]);
+  const [Stepts, addStep] = useState([{ componentId: 13, content: "helooo psychopath" }]);
+  
   console.log(Stepts);
+  
 
   function ClickAddStep() {
-    const id = 13;
+    id++ 
     const newSelect = [{ componentId: id, content: "" }];
 
     addStep(Stepts.concat(newSelect));
     console.log(Stepts);
   }
 
-  function ClickDeleteStep(place) {
-    addStep((SteptsBefore) => {
+  function ClickDeleteStep(componentId) {
+    addStep( ()=>{
+    const index = Stepts.findIndex((obj) => obj.componentId === componentId);
+    console.log(componentId)
+    console.log(index)
+    console.log(Stepts[index])
       return [
-        ...SteptsBefore.slice(0, place),
-        ...SteptsBefore.slice(place + 1),
-      ];
-    });
+        ...Stepts.slice(0, index),
+        ...Stepts.slice(index + 1),
+      ]});
   }
 
   function ChangeStepContent(componentId, content) {
+    ISdeteletd=true
+    console.log(content)
     const index = Stepts.findIndex((obj) => obj.componentId === componentId);
     Stepts[index].content = content;
     addStep(Stepts);
@@ -42,17 +51,22 @@ function StepFrom() {
 
   return (
     <div>
+    <Form.Label>Stepts</Form.Label>
       {Stepts.map((element, index) => {
         return (
           <div class="mb-2">
-            <Form.Label>Stepts</Form.Label>
             <InputGroup>
-              <InputGroup.Text>Step {element.componentId}.</InputGroup.Text>
+              <InputGroup.Text>Step {index}.</InputGroup.Text>
               <Form.Control
                 as="textarea"
                 onChange={(e) => {
                   ChangeStepContent(element.componentId, e.target.value);
                 }}
+                value={()=>{
+                    if(ISdeteletd){ 
+                        ISdeteletd=false 
+                        return element.content
+                    }}}
                 rows={2}
               />
               <DropdownButton
@@ -67,7 +81,7 @@ function StepFrom() {
               >
                 <Dropdown.Item
                   onClick={() => {
-                    ClickDeleteStep(index);
+                    ClickDeleteStep(element.componentId);
                   }}
                 >
                   Delete step
